1befeab5cec8f439ce6308269bbf030eda8f4780
Victor Borisik <victor.b@joblio.co>
2025-02-18 14:08:08 +0400

    add some outputs

--- update/run.sh
+++ update/run.sh
@@ -1,5 +1,9 @@
-cd $HOME && docker exec -i shardeum-dashboard /bin/bash -c "operator-cli stop"
-curl -O https://raw.githubusercontent.com/ViktarBarysik/shardeum/refs/heads/main/update/set-password.sh && mv set-password.sh shardeum/ && chmod +x shardeum/set-password.sh
-curl -O https://raw.githubusercontent.com/ViktarBarysik/shardeum/refs/heads/main/update/install.sh && chmod +x install.sh && ./install.sh
-docker exec -i shardeum-validator /bin/bash -c "operator-cli gui start && operator-cli start"
-docker exec -i shardeum-validator /bin/bash -c "pm2 list"
+cd $HOME && docker exec -i shardeum-dashboard /bin/bash -c "operator-cli stop";
+curl -O https://raw.githubusercontent.com/ViktarBarysik/shardeum/refs/heads/main/update/set-password.sh && mv set-password.sh shardeum/ && chmod +x shardeum/set-password.sh;
+echo "Starting installation"
+curl -O https://raw.githubusercontent.com/ViktarBarysik/shardeum/refs/heads/main/update/install.sh && chmod +x install.sh && ./install.sh;
+sleep 5
+echo "Starting operator-cli"
+docker exec shardeum-validator /bin/bash -c "operator-cli gui start && operator-cli start";
+echo "pm2 list"
+docker exec shardeum-validator /bin/bash -c "pm2 list";
