32ce60013b9de89f90926e6a2042014b4c47eed0
Victor Borisik <victor.b@joblio.co>
2025-02-18 14:12:12 +0400

    add additional version output

--- update/run.sh
+++ update/run.sh
@@ -1,9 +1,13 @@
+echo "get current version";
+docker exec shardeum-validator operator-cli status | grep activeVersion;
 cd $HOME && docker exec -i shardeum-dashboard /bin/bash -c "operator-cli stop";
 curl -O https://raw.githubusercontent.com/ViktarBarysik/shardeum/refs/heads/main/update/set-password.sh && mv set-password.sh shardeum/ && chmod +x shardeum/set-password.sh;
 echo "Starting installation"
 curl -O https://raw.githubusercontent.com/ViktarBarysik/shardeum/refs/heads/main/update/install.sh && chmod +x install.sh && ./install.sh;
-sleep 5
-echo "Starting operator-cli"
+sleep 5;
+echo "Starting operator-cli";
 docker exec shardeum-validator /bin/bash -c "operator-cli gui start && operator-cli start";
-echo "pm2 list"
+echo "pm2 list";
 docker exec shardeum-validator /bin/bash -c "pm2 list";
+echo "get current version";
+docker exec shardeum-validator operator-cli status | grep activeVersion;
