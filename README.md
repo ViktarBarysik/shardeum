563d20ef48336b0db0ba2d99f4680b1bbea1c743
Victor Borisik <victor.b@joblio.co>
2025-02-18 12:59:37 +0400

    add readme and fix run.sh commands

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # shardeum
+
+## update:
+run :
+```
+
+export SH_PASS=########
+curl -O https://raw.githubusercontent.com/ViktarBarysik/shardeum/refs/heads/main/update/install.sh
+
+```
--- update/run.sh
+++ update/run.sh
@@ -1,5 +1,5 @@
 cd $HOME && docker exec -i shardeum-dashboard /bin/bash -c "operator-cli stop"
-#curl -O https://raw.githubusercontent.com/shardeum/shardeum-validator/refs/heads/itn4/install.sh &&
-chmod +x install.sh && ./install.sh
+curl -O https://raw.githubusercontent.com/ViktarBarysik/shardeum/refs/heads/main/update/set-password.sh && mv set-password.sh shardeum/ && chmod +x shardeum/set-password.sh
+curl -O https://raw.githubusercontent.com/ViktarBarysik/shardeum/refs/heads/main/update/install.sh && chmod +x install.sh && ./install.sh
 docker exec -i shardeum-validator /bin/bash -c "operator-cli gui start && operator-cli start"
 docker exec -i shardeum-validator /bin/bash -c "pm2 list"
